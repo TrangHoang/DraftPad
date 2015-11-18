72c85f293222658f0dbc7b678996fb5638857cca
TrangHoang <tranghoangthuy89@gmail.com>
2015-11-18 23:05:03 +0700

    Add IRepository.cs and class Repository. Modify Program file.

--- Homework_One/Program.cs
+++ Homework_One/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Homework_One.Models;
 
 namespace Homework_One
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine(@"----Welcome to human resource management program---- 
             Choice one in all function below:
@@ -21,39 +19,140 @@ namespace Homework_One
                 7. Work with Database.
                 8. Exit.
                             ");
-
+            var listStaff = new Repositories<Staff>();
             Console.Write("Your number you choice: ");
-            int inputNumber = Convert.ToInt16(Console.ReadLine());
-            switch (inputNumber)
+            var inputNumber = Convert.ToInt32(Console.ReadLine());
+            // menu1
+            if (inputNumber == 1)
             {
-                case 1:
-                    Console.WriteLine("Choice 1 to enter office staff's information, or 2 to enter professor staff's information.");
-                    break;
-                case 2:
-                    Console.WriteLine("Choice 1 to search office staff's information, or 2 to search professor staff's information.");
-                    break;
-                case 3:
-                    Console.WriteLine("List staff's information.");
-                    break;
-                case 4:
-                    Console.WriteLine("Choice 1 to insert staff, 2 to edit staff, 3 to delete staff");
-                    break;
-                case 5:
-                    Console.WriteLine("Save information into a *.cvs file.");
-                    break;
-                case 6:
-                    Console.WriteLine("Read information from *.cvs file.");
-                    break;
-                case 7:
-                    Console.WriteLine("Work with database.");
-                    break;
-                case 8:
-                    Console.WriteLine("Bye!");                    
-                    break;
-                default:
-                    Console.WriteLine("Wrong number");
-                    Console.ReadKey();
-                    break;
+                Console.WriteLine(
+                 @"Choice 1 to enter office staff's information,
+                        or 2 to enter professor staff's information.");
+                var inputMenu1 = Convert.ToInt32(Console.ReadLine());
+                int typeStaff;
+                if (inputMenu1 == 1)
+                {
+                    typeStaff = 1;
+                    var staffEntity = AddStaff(typeStaff);
+                    listStaff.Add(staffEntity);
+                    return;
+                }
+                if (inputMenu1 == 2)
+                {
+                    typeStaff = 2;
+                    var staffEntity = AddStaff(typeStaff);
+                    listStaff.Add(staffEntity);
+                    return;
+                }
+                Console.WriteLine("Error");
+            }
+            // menu2
+            if (inputNumber == 2)
+            {
+                Console.WriteLine(
+                    "Choice 1 to search office staff's information, or 2 to search professor staff's information.");
+                var inputMenu2 = Convert.ToInt32(Console.ReadLine());
+                int typeStaff;
+                if (inputMenu2 == 1)
+                {
+                    typeStaff = 1;
+                   
+                    return;
+                }
+                if (inputMenu2 == 2)
+                {
+                    typeStaff = 2;
+                    return;
+                }
+                Console.WriteLine("Error");
+                return;
+            }
+            // menu3
+            if (inputNumber == 3)
+            {
+                Console.WriteLine("List staff's information.");
+                return;
+            }
+            // menu4
+            if (inputNumber == 4)
+            {
+                Console.WriteLine("Choice 1 to insert staff, 2 to edit staff, 3 to delete staff");
+                return;
+            }
+            // menu5
+            if (inputNumber == 5)
+            {
+                Console.WriteLine("Save information into a *.cvs file.");
+                return;
+            }
+            // menu6
+            if (inputNumber == 6)
+            {
+                Console.WriteLine("Read information from *.cvs file.");
+                return;
+            }
+            // menu7
+            if (inputNumber == 7)
+            {
+                Console.WriteLine("Work with database.");
+                return;
+            }
+            // menu8
+            if (inputNumber == 8)
+            {
+                Console.WriteLine("Bye!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Error.");
+                Console.ReadKey();
+            }
+        }
+
+        public static bool ExecuteMenuOne(Repositories<Staff> listStaff)
+        {
+            
+            return false;
+        }
+
+        // Process
+        private static Staff AddStaff(int typeStaff)
+        {
+            Console.Write("Full Name of staff office: ");
+            var fullName = Convert.ToString(Console.ReadLine());
+            Console.Write("Allowance of staff office: ");
+            var allowance = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Salary ratio of staff office: ");
+            var salaryRatio = Convert.ToDouble(Console.ReadLine());
+
+            if (typeStaff == 1)
+            {
+                Console.Write("Working time of staff office: ");
+                var workingTime = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Department of staff office: ");
+                var department = Convert.ToString(Console.ReadLine());
+                Console.Write("Position of staff office: ");
+                var position = Convert.ToString(Console.ReadLine());
+
+                Staff newOffice = new Office(new Guid(), fullName, allowance, salaryRatio, typeStaff,
+                    department, workingTime, position);
+                return newOffice;
+            }
+            if (typeStaff == 2)
+            {
+                Console.Write("Academi Degree of staff professor: ");
+                var academicDegree = Convert.ToString(Console.ReadLine());
+                Console.Write("Faculty of staff professor: ");
+                var faculty = Convert.ToString(Console.ReadLine());
+                Console.Write("Monthly Teaching time of staff professor: ");
+                float monthlyTeachingTime = Convert.ToInt64(Console.ReadLine());
+
+                Staff newProfessor = new Professor(new Guid(), fullName, allowance, salaryRatio, typeStaff,
+                    faculty, monthlyTeachingTime, academicDegree);
+                return newProfessor;
+            }
+            return null;
         }
     }
 }
---
+++ Homework_One/Repositories.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Homework_One
+{
+    public class Repositories<T> : IRepository<T> where T : class 
+    {
+       
+        
+        // Get all entities in a list
+        public IEnumerable<T> List { get; set; }
+        
+
+        public T GetById(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(List<T> entities)
+        {
+            var list = from item in entities select item;
+            return list;
+        }
+
+    }
+}
